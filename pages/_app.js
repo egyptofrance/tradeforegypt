d74aea11c3ef5ed14dff50ffb0d327f2b33b4ab6
Trade for Egypt Admin <admin@tradeforegypt.com>
2025-11-08 09:36:06 -0500

    ✨ إضافة خط Tajawal العربي
    - استخدام next/font/google لتحميل Tajawal
    - تطبيق الخط على كامل المشروع
    - دعم RTL بشكل افتراضي
    - أوزان متعددة (300, 400, 500, 700, 800)

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Tajawal } from 'next/font/google';
 import "swiper/css";
 import "swiper/css/navigation";
 import "react-modal-video/css/modal-video.css"
@@ -6,6 +7,12 @@ import '../public/assets/css/style.css';
 import '../styles/globals.css';
 import { useEffect, useState } from "react";
 import Preloader from '../components/elements/Preloader';
+
+const tajawal = Tajawal({
+  weight: ['300', '400', '500', '700', '800'],
+  subsets: ['arabic'],
+  display: 'swap',
+});
 
 function MyApp({ Component, pageProps }) {
     const [loading, setLoading] = useState(false);
@@ -23,6 +30,11 @@ function MyApp({ Component, pageProps }) {
     }, []);
     return (
         <>
+            <style jsx global>{`
+                html {
+                    font-family: ${tajawal.style.fontFamily};
+                }
+            `}</style>
             {!loading ? (
                 <Component {...pageProps} />
             ) : (
